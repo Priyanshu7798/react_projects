7c6fcb976e6e5e43ea85ecaf78a9eb6a7aab5055
Priyanshu7798 <124646393+Priyanshu7798@users.noreply.github.com>
2024-06-28 11:59:27 +0530

    Bg Changer react project

--- 01_bgChanger/src/App.jsx
+++ 01_bgChanger/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="box w-full h-screen" style={{backgroundColor:color}}>
+
+        <div className="navTab fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="tabCont flex flex-wrap bg-white gap-3 px-5 py-2 rounded-3xl shadow-xl">
+            <button onClick={()=> setColor('red')} className='outline-none px-4 py-1 text-white rounded-3xl shadow-lg'style={{backgroundColor:'red'}}>
+            Red</button>
+            <button onClick={()=> setColor('green')} className='outline-none px-4 py-1 text-white rounded-3xl shadow-lg'style={{backgroundColor:'green'}}>
+            Green</button>
+            <button onClick={()=> setColor('blue')} className='outline-none px-4 py-1 text-white rounded-3xl shadow-lg'style={{backgroundColor:'blue'}}>
+            Blue</button>
+          </div>
+        </div>  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
